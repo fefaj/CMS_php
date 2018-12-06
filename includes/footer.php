230a8a20b143cf8876221b24e5b02a26f4334664
fefaj <fefaj@users.noreply.github.com>
2018-12-06 18:53:52 +0100

    one more test updated

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
  <footer>
             <div class="row">
                 <div class="col-lg-12">
-                    <p>Copyright &copy; fefadev.com </p>
+                    <p>Copyright for my web &copy; fefadev.com </p>
                 </div>
                 <!-- /.col-lg-12 -->
             </div>
